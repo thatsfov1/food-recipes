d7697e1a5e7acf3acddbba7ef5c2e927862df93d
thatsfov1 <zheniaukr@icloud.com>
2023-02-15 18:55:11 +0200

    removed

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,8 +3,8 @@ import s from './Navbar.module.css'
 import {NavLink} from 'react-router-dom'
 
 const Navbar: React.FC = (props): React.ReactElement => {
-    return <div className='p-25'>
-        <div className='flex align-'>
+    return <div className={s.container}>
+        <div className={s.nav}>
           <NavLink to='/'>Home</NavLink>
           <NavLink to='/category'>Categories</NavLink>
           <NavLink to='/'>Recipes</NavLink>
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import s from './Home.module.css'
+
+const Home: React.FC = ()=> {
+    return <div className={s.container}>
+
+    </div>;
+}
+
+export default Home;
